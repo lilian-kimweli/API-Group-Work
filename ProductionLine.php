c0a7d2724af1fd43d5f38cf6625afe56d032b26c
lilian-kimweli <lilian.kimweli@strathmore.edu>
2025-09-26 08:43:02 +0300

    Create method

--- ProductionLine.php
+++ ProductionLine.php
@@ -7,8 +7,25 @@ class ProductionLine {
     public $name;
     public $classification;
 
-    public function __construct($db) {
-        $this->conn = $db;
+ public function create() {
+        $query = "INSERT INTO " . $this->table_name . " 
+                  SET name=:name, classification=:classification";
+
+        $stmt = $this->conn->prepare($query);
+
+        // Sanitize input
+        $this->name = htmlspecialchars(strip_tags($this->name));
+        $this->classification = htmlspecialchars(strip_tags($this->classification));
+
+        // Bind parameters
+        $stmt->bindParam(":name", $this->name);
+        $stmt->bindParam(":classification", $this->classification);
+
+        if($stmt->execute()) {
+            return true;
+        }
+        return false;
     }
 }
+
 ?>
