1df0a23f0be2bb1634b1a39c75cb7a6f37559284
kaburu-faith <fwangari229@gmail.com>
2025-09-26 09:10:44 +0300

    readOne and update methods

--- Lot.php
+++ Lot.php
@@ -24,4 +24,28 @@ class Lot {
         $stmt->execute();
         return $stmt;
     }
+    function readOne() {
+        $query = "SELECT id, name FROM " . $this->table_name . " WHERE id = ? LIMIT 0,1";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(1, $this->id);
+        $stmt->execute();
+
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        if($row) {
+            $this->name = $row['name'];
+            return true;
+        }
+        return false;
+    }
+
+    function update() {
+        $query = "UPDATE " . $this->table_name . " SET name=:name WHERE id=:id";
+        $stmt = $this->conn->prepare($query);
+        $this->name = htmlspecialchars(strip_tags($this->name));
+        $this->id = htmlspecialchars(strip_tags($this->id));
+
+        $stmt->bindParam(":name", $this->name);
+        $stmt->bindParam(":id", $this->id);
+        return $stmt->execute();
+    }
 ?>
