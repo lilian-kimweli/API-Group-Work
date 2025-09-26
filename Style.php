48fa5656da32b570afe925f29426e843c59f2686
kaburu-faith <fwangari229@gmail.com>
2025-09-26 09:08:19 +0300

    readOne and update methods

--- Style.php
+++ Style.php
@@ -35,4 +35,40 @@ class Style {
         $stmt->execute();
         return $stmt;
     }
+
+    function readOne() {
+        $query = "SELECT s.id, s.name, s.production_line_id, p.name as production_line_name
+                  FROM " . $this->table_name . " s
+                  LEFT JOIN production_lines p ON s.production_line_id = p.id
+                  WHERE s.id = ? LIMIT 0,1";
+
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(1, $this->id);
+        $stmt->execute();
+
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        if($row) {
+            $this->name = $row['name'];
+            $this->production_line_id = $row['production_line_id'];
+            return true;
+        }
+        return false;
+    }
+
+     function update() {
+        $query = "UPDATE " . $this->table_name . " 
+                  SET name=:name, production_line_id=:production_line_id 
+                  WHERE id=:id";
+
+        $stmt = $this->conn->prepare($query);
+        $this->name = htmlspecialchars(strip_tags($this->name));
+        $this->production_line_id = htmlspecialchars(strip_tags($this->production_line_id));
+        $this->id = htmlspecialchars(strip_tags($this->id));
+
+        $stmt->bindParam(":name", $this->name);
+        $stmt->bindParam(":production_line_id", $this->production_line_id);
+        $stmt->bindParam(":id", $this->id);
+
+        return $stmt->execute();
+    }
 ?>
