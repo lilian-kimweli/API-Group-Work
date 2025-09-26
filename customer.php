b082050617ce9e58dcd82b6947f574cb149eee2b
ROSEMARYWANJIRU13 <rosemary.wainaina@strathmore.edu>
2025-09-26 09:57:35 +0300

    add create and read methods to Customer class

--- customer.php
+++ customer.php
@@ -12,5 +12,33 @@ class Customer {
     public function __construct($db) {
         $this->conn = $db;
     }
+    public function create() {
+        $query = "INSERT INTO " . $this->table_name . " 
+                  SET name=:name, address=:address, telephone=:telephone, email=:email";
+
+        $stmt = $this->conn->prepare($query);
+        
+        $this->name = htmlspecialchars(strip_tags($this->name));
+        $this->address = htmlspecialchars(strip_tags($this->address));
+        $this->telephone = htmlspecialchars(strip_tags($this->telephone));
+        $this->email = htmlspecialchars(strip_tags($this->email));
+
+        $stmt->bindParam(":name", $this->name);
+        $stmt->bindParam(":address", $this->address);
+        $stmt->bindParam(":telephone", $this->telephone);
+        $stmt->bindParam(":email", $this->email);
+
+        return $stmt->execute();
+    }
+
+    public function read() {
+        $query = "SELECT id, name, address, telephone, email 
+                  FROM " . $this->table_name . " 
+                  ORDER BY name ASC";
+        
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+        return $stmt;
+    }
 }
 ?>
