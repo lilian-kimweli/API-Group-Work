60b31c587a396ee156cc2625b719bd36f2059ec6
kaburu-faith <fwangari229@gmail.com>
2025-09-26 09:06:16 +0300

    create and read methods

--- Lot.php
+++ Lot.php
@@ -10,4 +10,18 @@ class Lot {
         $this->conn = $db;
     }
 }
+    function create() {
+        $query = "INSERT INTO " . $this->table_name . " SET name=:name";
+        $stmt = $this->conn->prepare($query);
+        $this->name = htmlspecialchars(strip_tags($this->name));
+        $stmt->bindParam(":name", $this->name);
+        return $stmt->execute();
+    }
+
+     function read() {
+        $query = "SELECT id, name FROM " . $this->table_name . " ORDER BY name ASC";
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+        return $stmt;
+    }
 ?>
