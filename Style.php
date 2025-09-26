fcd24c35adb5b5f7e0135b8269d3293ee0477bb9
kaburu-faith <fwangari229@gmail.com>
2025-09-26 09:03:41 +0300

    create and read method

--- Style.php
+++ Style.php
@@ -11,4 +11,28 @@ class Style {
         $this->conn = $db;
     }
 }
+    function create() {
+        $query = "INSERT INTO " . $this->table_name . " 
+                  SET name=:name, production_line_id=:production_line_id";
+
+        $stmt = $this->conn->prepare($query);
+        $this->name = htmlspecialchars(strip_tags($this->name));
+        $this->production_line_id = htmlspecialchars(strip_tags($this->production_line_id));
+
+        $stmt->bindParam(":name", $this->name);
+        $stmt->bindParam(":production_line_id", $this->production_line_id);
+
+        return $stmt->execute();
+    }
+
+     function read() {
+        $query = "SELECT s.id, s.name, p.name as production_line_name 
+                  FROM " . $this->table_name . " s
+                  LEFT JOIN production_lines p ON s.production_line_id = p.id
+                  ORDER BY s.name ASC";
+
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+        return $stmt;
+    }
 ?>
