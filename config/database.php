b4c3ab7bd7e46c60e2934f62e9d6be93dac7eae2
faith-loise <167431752+faith-loise@users.noreply.github.com>
2025-10-25 14:58:02 +0300

    updated db

--- config/database.php
+++ config/database.php
@@ -5,6 +5,7 @@ define('DB_NAME', 'bluebell_db');
 define('DB_USER', 'root');
 define('DB_PASS', 'eunice'); 
 
+
 // Website configuration
 define('SITE_URL', 'http://localhost/blubellseek/public/');
 ?>
