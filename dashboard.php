25774780d86997947d8f68f41235d2451cf65bac
EuniceMungai <eunice.mungai@strathmore.edu>
2025-10-25 14:34:02 +0300

    initial

--- dashboard.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-if(!isset($_SESSION['logged_in'])){
-    header("Location: login.php");
-}
-?>
-
-<h2>Welcome, <?= $_SESSION['logged_in'] ?>!</h2>
-<a href="logout.php">Logout</a>
